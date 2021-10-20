53939fd0a49ee0cc43dffee80d91c295bc06117a
irfan-fauzi@yahoo.com <irfanwintech@yahoo.com>
2021-10-20 11:07:17 +0700

    movie

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,17 +1,14 @@
 /* eslint-disable linebreak-style */
+/* eslint-disable import/extensions */
 /* eslint-disable import/no-extraneous-dependencies */
-/* eslint-disable linebreak-style */
 const { merge } = require('webpack-merge')
-// eslint-disable-next-line import/extensions
-const path = require('path')
-// eslint-disable-next-line import/extensions
 const common = require('./webpack.common.js')
 
 module.exports = merge(common, {
   mode: 'development',
-  devServer: {
-    contentBase: path.resolve(__dirname, 'dist'),
-  },
+  // devServer: {
+  //   contentBase: path.resolve(__dirname, 'dist'),
+  // },
   module: {
     rules: [
       {
