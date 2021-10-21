c5cfacd358da244a19d761e9ca272cabe0d186b5
irfan-fauzi@yahoo.com <irfanwintech@yahoo.com>
2021-10-21 10:36:11 +0700

    class drawer

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -2,5 +2,3 @@
 /* eslint-disable import/no-unresolved */
 import 'regenerator-runtime'
 import '../styles/main.css'
-
-console.log('ok')
---
+++ src/scripts/views/app.js
@@ -0,0 +1,15 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable no-underscore-dangle */
+class App {
+  constructor({ button, drawer, content }) {
+    this._button = button
+    this._drawer = drawer
+    this._content = content
+  }
+
+  _initialAppShell(){
+     
+  }
+}
+
+export default App
